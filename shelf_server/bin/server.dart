7d2267b285cd4e40a0644477edc0106ebd19d547
suragch <suragch@suragch.dev>
2024-01-31 16:24:49 +0800

    initial commit

---
+++ shelf_server/bin/server.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_server/request_handlers.dart';
+
+final _router = Router()
+  ..post('/create', createHandler)
+  ..get('/read', readHandler)
+  ..put('/update', updateHandler)
+  ..delete('/delete', deleteHandler);
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
---
+++ shelf_server/lib/request_handlers.dart
@@ -0,0 +1,17 @@
+import 'package:shelf/shelf.dart';
+
+Response createHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response readHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response updateHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response deleteHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
